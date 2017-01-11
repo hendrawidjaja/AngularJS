337a4b86997f4569ef6dfc633cf1b1cfba4fdaf8
Hendra Widjaja <hendrawidjaja@live.de>
2017-01-11 18:38:02 +0100

    Adding filter which got value from the input tag

--- app/app.js
+++ app/app.js
@@ -1,16 +1,24 @@
 var myApp = angular.module("myApp", []);
 
-/* this config runs first before any other function */
-myApp.config(function(){
-
-});
-
-myApp.run(function(){
-
-});
-
 myApp.controller("myAppController", ["$scope", function($scope){
     /* scope object */
     $scope.message = "Hello";
-    $scope.cities = ["New York", "London", "Paris", "Tokyo", "HongKong"];
+    $scope.cities = [
+        {
+            Name: "New York",
+            Food: "Hot dog"
+        }, {
+            Name: "London",
+            Food: "Fish&Chips"
+        }, {
+            Name: "Paris",
+            Food: "Baguette"
+        }, {
+            Name: "Tokyo",
+            Food: "Sushi"
+        }, {
+            Name: "HongKong",
+            Food: "Roast Goose"
+        }
+    ];
 }]);
