2bfee62270d49940bf87332c1b944c29f3a80419
Hendra Widjaja <hendrawidjaja@live.de>
2017-01-12 19:50:54 +0100

    Binding ng-click and use it to sort item inside Listitem

--- app/app.js
+++ app/app.js
@@ -1,28 +1,48 @@
 var myApp = angular.module("myApp", []);
 
 myApp.controller("myAppController", ["$scope", function($scope){
+    /* a function to handle clickEvent of remove */
+    $scope.removeItem = function(item) {
+        var deleteItem = $scope.cities.indexOf(item); /* get index of item */
+        $scope.cities.splice(deleteItem, 1); /* remove the item from the array */
+    }
     /* scope object */
     $scope.message = "Hello";
     $scope.cities = [
         {
             Name: "New York",
             Food: "Hot dog",
+            Price: 5,
             display: true
         }, {
             Name: "London",
             Food: "Fish & Chips",
+            Price: 8,
             display: true
         }, {
             Name: "Paris",
             Food: "Baguette",
+            Price: 6,
             display: true
         }, {
             Name: "Tokyo",
             Food: "Sushi",
-            display: false
+            Price: 12,
+            display: true
         }, {
-            Name: "HongKong",
+            Name: "Hong Kong",
             Food: "Roast Goose",
+            Price: 30,
+            display: true
+        }, {
+            Name: "Thailand",
+            Food: "Tom Yum",
+            Price: 3,
+            display: true
+        }, {
+            Name: "Indonesia",
+            Food: "Nasi Goreng",
+            Price: 2,
             display: true
         }
     ];
